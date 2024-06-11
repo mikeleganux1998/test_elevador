9709f150e809b3279fff74ae2b632c455aad635c
Miguel Rosas <111320389+mikeleganux1998@users.noreply.github.com>
2024-06-10 23:04:18 -0600

    update files

--- common.h
+++ common.h
@@ -1,5 +1,10 @@
 #ifndef COMMON_H
 #define COMMON_H
+#define ROW1 PORTAbits.RA0
+#define ROW2 PORTAbits.RA1
+#define ROW3 PORTAbits.RA2
+#define ROW4 PORTAbits.RA3
+
 
 #define _XTAL_FREQ 8000000  // Frecuencia del oscilador
 
--- main.c
+++ main.c
@@ -2,51 +2,45 @@
 #include "common.h"
 #include "lcd.h"
 #include "motor_led.h"
+#include <stdio.h> // Para utilizar sprintf
+#include <string.h> // Para utilizar strlen
 
-#define _XTAL_FREQ 8000000  // Frecuencia del oscilador
+// Define la velocidad de la UART
+#define BAUDRATE 9600
+#define BRGVAL ((unsigned int)(_XTAL_FREQ/(16UL * BAUDRATE) - 1))
 
-// Configuración de los fusibles para el PIC18F4550
-#pragma config FOSC = HSPLL_HS
-#pragma config PLLDIV = 5
-#pragma config CPUDIV = OSC1_PLL2
-#pragma config USBDIV = 2
-#pragma config FCMEN = OFF
-#pragma config IESO = OFF
-#pragma config PWRT = ON
-#pragma config BOR = ON
-#pragma config BORV = 3
-#pragma config VREGEN = ON
-#pragma config WDT = OFF
-#pragma config WDTPS = 32768
-#pragma config CCP2MX = ON
-#pragma config PBADEN = OFF
-#pragma config LPT1OSC = OFF
-#pragma config MCLRE = ON
-#pragma config STVREN = ON
-#pragma config LVP = OFF
-#pragma config ICPRT = OFF
-#pragma config XINST = OFF
-#pragma config DEBUG = OFF
+// Prototipo de función para inicializar la UART
+void init_uart(void);
 
-// Definiciones para el teclado matricial
-#define ROW1 PORTAbits.RA0
-#define ROW2 PORTAbits.RA1
-#define ROW3 PORTAbits.RA2
-#define ROW4 PORTAbits.RA3
-#define COL1 PORTBbits.RB0
-#define COL2 PORTBbits.RB1
-#define COL3 PORTBbits.RB2
-#define COL4 PORTBbits.RB3
+// Función para enviar una cadena a través de la UART
+void uart_send_string(const char* str);
 
-// Prototipos de funciones
-char read_keypad(void);
+char read_keypad(void) {
+    char key = '\0';
+    for (int col = 0; col < 4; col++) {
+        TRISB = 0xF0 | (1 << col);
+        LATB = ~(1 << col);
+        
+        __delay_ms(20);  // Pequeño retardo para permitir la estabilización de la señal
+
+        if (ROW1 == 0) key = '1' + col - 0;
+        if (ROW2 == 0) key = '4' + col - 1;
+        if (ROW3 == 0) key = '7' + col - 2;
+        if (ROW4 == 0) key = '*' + col - 3;
+
+        if (key != '\0') break;
+    }
+    return key;
+}
 
 void main(void) {
     TRISA = 0x0F;  // RA0-RA3 como entrada (filas del teclado)
     TRISB = 0xF0;  // RB0-RB3 como salida (columnas del teclado) y RB4-RB7 como entrada (sensores)
-    
+    TRISD = 0x00;  // Puerto D como salida (motor paso a paso y LEDs)
+
     lcd_init();  // Inicializar el LCD
     init_leds_and_motor();  // Inicializar LEDs y motor
+    init_uart(); // Inicializa la UART para la comunicación con el Data Visualizer
 
     int current_level = 0;
     display_level(current_level);  // Mostrar el nivel inicial en LEDs y LCD
@@ -66,25 +60,28 @@ void main(void) {
                 lcd_set_cursor(1, 1);
                 lcd_write_string("Nivel: ");
                 lcd_write_char(current_level + '0');
+
+                // Envía el nivel actual a través de la UART para el Data Visualizer
+                char buffer[20];
+                sprintf(buffer, "Nivel: %d\r\n", current_level);
+                uart_send_string(buffer);
             }
         }
     }
 }
 
-char read_keypad(void) {
-    char key = '\0';
-    for (int col = 0; col < 4; col++) {
-        TRISB = 0xF0 | (1 << col);
-        LATB = ~(1 << col);
-        
-        __delay_ms(20);  // Pequeño retardo para permitir la estabilización de la señal
 
-        if (ROW1 == 0) key = '1' + col;
-        if (ROW2 == 0) key = '4' + col;
-        if (ROW3 == 0) key = '7' + col;
-        if (ROW4 == 0) key = '*' + col;
 
-        if (key != '\0') break;
+void init_uart(void) {
+    SPBRG = BRGVAL;             // Establece la velocidad de la UART
+    TXSTAbits.BRGH = 1;         // Velocidad alta
+    RCSTAbits.SPEN = 1;         // Habilita el puerto serie
+    TXSTAbits.TXEN = 1;         // Habilita la transmisión
+}
+
+void uart_send_string(const char* str) {
+    while (*str) {
+        while (!TXSTAbits.TRMT);    // Espera a que el buffer de transmisión esté vacío
+        TXREG = *str++;             // Envía un carácter
     }
-    return key;
 }
